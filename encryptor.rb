92de31cff415d1fe29f2d34a998786a5a86b966b
Nilesh <nileshkumar.be@gmail.com>
2018-06-19 06:12:18 +0000

    expolre ruby concepts

---
+++ encryptor.rb
@@ -0,0 +1,38 @@
+class Encryptor
+
+  def cypher
+    {
+      'a'=>'n', 'b'=>'o', 'c'=>'p', 'd'=>'q',
+      'e'=>'r','f'=>'s','g'=>'t','h'=>'u',
+      'i'=>'v','j'=>'w','k'=>'x','l'=>'y',
+      'm'=>'z','n'=>'a', 'o'=>'b', 'p'=>'c',
+      'q'=>'d', 'r'=>'e', 's'=>'f', 't'=>'g',
+      'u'=>'h', 'v'=>'i', 'w'=>'j', 'x'=>'k',
+      'y'=>'l', 'z'=>'m', ""=> ' '
+    }
+  end
+
+  def encrypt_letter(letter)
+    lowercase_letter = letter.downcase
+    cypher[lowercase_letter]
+  end
+
+  def encrypt(string)
+    encrypted_word = []
+    #1. split the word
+    words = string.split
+    words.each do |word|
+      letter = word.split("")
+      word = letter.collect do |l|
+        encrypt_letter(l)
+      end
+      encrypted_word << word
+
+    end
+    # words = []
+    encrypted_word.map do |a|
+      a.join
+    end.join(" ")
+  end
+
+end
